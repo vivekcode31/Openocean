ab810b59f0992e476a595db3c51b4484622e05e0
Vivek kumar <vivekvipar18@gmail.com>
2025-06-18 16:02:17 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,12 +4,11 @@ module.exports = {
   
   solidity: "0.8.4",
   paths: {
-    
+  
     artifacts: "./src/backend/artifacts",
     sources: "./src/backend/contracts",
     cache: "./src/backend/cache",
     tests: "./src/backend/test"
-    
     
   },
 };
